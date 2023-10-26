8cc36f799f710717190e3e3f7a509a973c9ed469
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2023-10-26 17:19:48 +0700

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
   <router-view />
 </template>
-
+<script>
+import { HEART } from './constants/image';
+export default {
+  name: 'UserLayout',
+  created() {
+    this.HEART = HEART;
+  },
+  data() {
+    return {
+      numberNotifications: 0,
+    };
+  },
+  mounted() {
+    this.$notification.open({
+      message: 'Notification',
+      description: 'You have a new notification',
+      style: {
+        width: '400px',
+      },
+    });
+  },
+};
+</script>
 <style lang="scss">
+.ant-notification-notice-message {
+  font-weight: 600;
+  font-size: 18px !important;
+}
+.ant-notification-notice-description {
+  font-size: 16px !important;
+}
 body {
   ::selection {
     background-color: #f05123;
